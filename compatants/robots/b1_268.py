0acf4f3ed9f7893641b9ae4b6dcbba1fafa4c1e1
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-02-20 12:31:29 -0500

    🐛 FIX: removed an attribute not related to class

--- compatants/robots/b1_268.py
+++ compatants/robots/b1_268.py
@@ -7,7 +7,6 @@ class B1_268(Robot):
     def __init__(self) -> None:
         self.model = 'B1-268'
         self.modifiers = robots_models[self.model]['mods']
-        self.evasion = self.modifiers['evade']
         super().__init__()
 
     def weapon_stats(self):
