e33787779366ea1929eef8289c38717d569b3188
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-02-20 14:43:50 -0500

    📦 NEW: Implement new classes into battlefield

--- Battlefield.py
+++ Battlefield.py
@@ -1,14 +1,31 @@
-from compatants.dinosaurs.t_rex import TRex
-from compatants.dinosaurs.stegosurus import Stegosurus as Steg
-from compatants.dinosaurs.pterodactyl import Pterodactyl as ptero
-from compatants.robots.gen2_sparring_bot import Gen2SparingBot as G2
-from compatants.robots.e54 import E54
-from compatants.robots.b1_268 import B1_268
+from compatants.compatant import Compatant as com
+from compatants.dinosaurs.herd import Herd
+from compatants.robots.fleet import Fleet
+from compatants.robots.robot import Robot
+from compatants.dinosaurs.dinosaur import Dinosaur as Dino
+import random as r
 
 class Battlefield:
     def __init__(self) -> None:
-        self.robot = B1_268()
-        self.dinosaur = Steg()
+        even_teams = input('Would you like to make even teams? [y,n] : ')
+        if even_teams.lower() == 'y':
+            team_size = input('Would you like set the team size? or at random between 3 to 5? [#, random]')
+            if team_size.lower() == 'random':
+                team_size = r.randrange(3,6)
+                self.herd = Herd(team_size)
+                self.fleet = Fleet(team_size)
+            else:
+                self.herd = Herd(int(team_size))
+                self.fleet = Fleet(int(team_size))
+        elif even_teams.lower() == 'n':
+            team_size = input('Would you like set the team size? or at random between 3 to 5? [set, random]')
+            if team_size.lower() == 'random':
+                self.herd = Herd(r.randrange(3,6))
+                self.fleet = Fleet(r.randrange(3,6))
+            else:
+                self.herd = Herd(int(input('Dino Team: ')))
+                self.fleet = Fleet(int(input('Robot Team: ')))
+        self.battle_order = com.in_order_group(self, self.herd.herd, self.fleet.fleet)
         self.round = 1
 
     def run_game(self):
@@ -20,13 +37,24 @@ class Battlefield:
         print('\nWelcome to an epic battle for the ages!\nOnly one side can win!')
 
     def battle_phase(self):
-        while self.robot.health != 0 and self.dinosaur.health != 0:
+        while len(self.herd.herd) != 0 and len(self.fleet.fleet) != 0:
             print(f'---------- Round {self.round} ----------')
-            self.robot.attack(self.dinosaur)
-            if self.dinosaur.health == 0:
-                break
-            print()
-            self.dinosaur.attack(self.robot)
+            for compatant in self.battle_order:
+                if isinstance(compatant, Robot):
+                    opponent = r.choice(self.herd.herd)
+                    compatant.attack(opponent)
+                    if opponent.health <= 0:
+                        self.herd.herd.remove(opponent)
+                        if len(self.herd.herd) == 0:
+                            break
+                elif isinstance(compatant, Dino):
+                    opponent = r.choice(self.fleet.fleet)
+                    compatant.attack(opponent)
+                    if opponent.health <= 0:
+                        self.fleet.fleet.remove(opponent)
+                        if len(self.fleet.fleet) == 0:
+                            break
+                print()
             self.round += 1
 
     def display_winner(self):
